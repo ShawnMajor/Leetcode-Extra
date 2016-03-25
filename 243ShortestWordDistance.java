344ec84ac3e5ca37b971156e5c920848d9de0fba
ShawnMajor <garnett.sx@gmail.com>
2016-03-25 12:06:46 -0400

    Update and rename 243_Shortest_Word_Distance to 243ShortestWordDistance.java

--- 243ShortestWordDistance.java
+++ 243ShortestWordDistance.java
@@ -1,4 +1,4 @@
-public class Solution {
+public class 243ShortestWordDistance {
     public int shortestDistance(String[] words, String word1, String word2) {
         int idx1 = -1, idx2 = -1, distance = Integer.MAX_VALUE;
         for(int i = 0; i < words.length; i++){

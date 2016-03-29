abb4e26346ded3d72696b1ad090aa1608c537cc6
ShawnMajor <garnett.sx@gmail.com>
2016-03-29 12:02:48 -0400

    Update 267PalindromePermutation2.java

--- 267PalindromePermutation2.java
+++ 267PalindromePermutation2.java
@@ -5,49 +5,84 @@ public class 267PalindromePermutation {
             return results;
             
         Map<Character, Integer> d = new HashMap<Character, Integer>();
-        for(int i = 0; i < s.length(); i++) {
+        
+        for (int i = 0; i < s.length(); i++) {
             if(d.containsKey(s.charAt(i)))
                 d.put(s.charAt(i), d.get(s.charAt(i)) + 1);
             else
                 d.put(s.charAt(i), 1);
         }
         
-        String candidate = "";
-        String single = "";
-        boolean already = false;
+        StringBuilder candidate = new StringBuilder();
+        char single = '\0';
+        boolean oddCount = false;
         
-        for(Character c : d.keySet()) {
-            int num = d.get(c) / 2;
-            for(int i = 0; i < num; i++)
-                candidate += c;
-            if(d.get(c) % 2 != 0) {
-                if(already)
+        for (Character c : d.keySet()) {
+            if (d.get(c) % 2 != 0) {
+                if (oddCount)
                     return results;
                 else {
-                    already = true;
-                    single += c;
+                    oddCount = true;
+                    single = c;
                 }
+            for(int i = 0; i < d.get(c) / 2; i++)
+                candidate.append(c);
             }
         }
         
-        if(candidate.length() == 0 && single.length() != 0) {
-            results.add(single);
+        if (candidate.length() == 0 && oddCount) {
+            results.add(String.valueOf(single));
             return results;
         }
         
-        recursion("", candidate, single, candidate.length(), results);
+        permutation(cadidate.toString.toCharArray, 0, results);
+        
+        for (int i = 0; i < results.size(); i++) {
+            str = results.get(i);
+            StringBuilder sb = new StringBuilder(str);
+            if (oddCount)
+                sb.append(single);
+            for (int j = str.length() - 1; j >= 0; j--) {
+                sb.append(str.charAt(j));
+            }
+            results.set(i, sb.toString());
+        }
         return results;
     }
-    
-    private void recursion(String left, String candidate, String single, int l, List<String> results) {
-        if(left.length() == l) {
-            String right = new StringBuffer(left).reverse().toString();
-            results.add(left + single + right);
+
+    private void permutation(char[] s, int start, List<String> result) {
+        if (start >= s.length) {
+            result.add(new String(s));
+            return;
         }
-        for(int i = 0; i < candidate.length(); i++) {
-            if(i > 0 && candidate.charAt(i) == candidate.charAt(i - 1))
-                continue;
-            recursion(left + candidate.charAt(i), candidate.substring(0, i) + candidate.substring(i + 1), single, l, results);
+         
+        for (int i = start; i < s.length; i++) {
+            if (!containsDuplicate(s, start, i)) {
+                swap(s, i, start);
+                permutation(s, start + 1, result);
+                swap(s, i, start);
+            }
         }
     }
+     
+    private void swap(char[] s, int i, int j) {
+        char temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+    }
+     
+    private boolean containsDuplicate(char[] s, int start, int end) {
+        for (int i = start; i < end; i++) {
+            if (s[i] == s[end]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
+    
+    
+    
+    
+
